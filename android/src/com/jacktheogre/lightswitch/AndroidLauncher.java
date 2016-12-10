72ac049536fb27313ccfe388c092450f0270ae87
ImmortalTurtle <immortalsuperturtle@gmail.com>
2016-12-10 13:42:55 +0300

    Project setup

---
+++ android/src/com/jacktheogre/lightswitch/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jacktheogre.lightswitch;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jacktheogre.lightswitch.LightSwitch;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LightSwitch(), config);
+	}
+}
